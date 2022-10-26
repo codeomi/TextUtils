604f9973f636aa9e9a0fd504c05d93c27d5f4754
Omkar Kudale <omkarkudale007@gmail.com>
2022-10-26 19:54:29 +0530

    changed the styling of navbar and added preview text

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,9 +2,8 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 
-
 export default function TextForm(props) {
-    const[text, setText] = useState('Enter text here again.')
+    const[text, setText] = useState(' ')
     // text = "omkar" wrong way to chanege the vlaue of variable
     // setText("omkar")
 
@@ -20,12 +19,10 @@ export default function TextForm(props) {
     }
 
 
-
     const handleOnChange = (event)=>{
         console.log("onChange");
         setText(event.target.value)
     }
-
 
 
     return (
@@ -34,17 +31,19 @@ export default function TextForm(props) {
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <div className="container">
-                <textarea className="form-control" id="myBox" value= {text}  onChange = {handleOnChange} rows="4"></textarea>
+                <textarea className="form-control" id="myBox" value= {text}  onChange = {handleOnChange} rows="8"></textarea>
                 </div>
                 <div className="btn btn-primary my-3 m-3"  onClick={handleUpClick} >Convert to UpperCase</div>
                 <div className="btn btn-primary my-3" onClick={handleSmallClick}>Convert to SmallerCase</div>
             </div>
         </div>
         <div className="container m-2">
-            <h1>Your text summary</h1>
-            <p>Your words: <span>{text.split(" ").length}</span></p>
-            <p>Your characters: <span>{text.length}</span>
-            </p>
+            <h2>Your text summary</h2>
+            <p><b>Your words: </b><span>{text.split(" ").length}</span></p>
+            <p><b>Your characters: </b> <span>{text.length}</span></p>
+            <p><b>Time to read: </b> {((text.split(" ").length * 0.2)/60).toFixed(2)} minutes</p>
+            <h3>Preview : </h3>
+            <p>{text}</p>
         </div>
         </>
     )
