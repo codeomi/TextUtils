49dfeb7f9aed04381b4339be581998ef6758a031
Omkar Kudale <omkarkudale007@gmail.com>
2022-11-03 12:13:36 +0530

    Added router component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import './Appa.css';
 import Alert from './components/Alert';
-// import About from './components/About';
 import NavBar from './components/NavBar';
 import TextArea from './components/TextForm';
-
+import About from './components/About'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light")//tells whether dark mode is enabled or not
   const [alert, setAlert] = useState(null)
-  const[lightButton,setlightButton]= useState(true)
-  const[darkButton,setdarkButton]= useState(false)
+  const [lightButton, setlightButton] = useState(true)
+  const [darkButton, setdarkButton] = useState(false)
 
   //this function helps us to show alert msg
   const showAlert = (message, type) => {
@@ -24,13 +29,13 @@ function App() {
 
 
   const toggleMode = () => {
-    if (mode==="light") {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#57518b"
       showAlert("Dark mode hs been enabled", "success")
       setlightButton(false)
       setdarkButton(true)
-       }
+    }
     else {
       setMode("light")
       document.body.style.backgroundColor = "white"
@@ -41,12 +46,13 @@ function App() {
   }
 
   const toggleGreenMode = () => {
-    if (mode==="light") {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#0c4224"
       showAlert("Green mode hs been enabled", "success")
       setlightButton(false)
       setdarkButton(true)
+      document.title = "TextUtils - Dark mode"
     }
     else {
       setMode("light")
@@ -59,12 +65,19 @@ function App() {
 
   return (
     <>
-      <NavBar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} toggleGreenMode={toggleGreenMode}/>
-      <Alert alert={alert} />
-      <div className="container my-3" >
-        <TextArea showAlert={showAlert} heading="Enter the text to analyse" mode={mode}  />
-        {/* <About/> */}
-      </div>
+      <Router>
+        <NavBar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} toggleGreenMode={toggleGreenMode} />
+        <Alert alert={alert} />
+        <div className="container my-3" >
+          <Routes>
+            <Route exact path="/about" element={<About />}>
+            </Route>
+            <Route exact path="/" element={<TextArea showAlert={showAlert} heading="Enter the text to analyse" mode={mode} />
+            }>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
